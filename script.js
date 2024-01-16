cd44f4e5f6de000c4609aad72ea4dd76800a72ef
macbook air <macbookair@macbooks-MacBook-Air-2.local>
2024-01-17 03:55:12 +0400

    logic done, mobile friendly css

--- script.js
+++ script.js
@@ -7,9 +7,10 @@
 // document.querySelector(".guess").value = 21;
 // console.log(document.querySelector(".guess").value)
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener
 ("click", function() {
@@ -20,6 +21,15 @@ document.querySelector('.check').addEventListener
         document.querySelector('.message').textContent = "no number selected"
     } else if(guess === secretNumber){
         document.querySelector('.message').textContent = "Correct number";
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = "#60b347"
+        document.querySelector('.number').style.width = "30rem"
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector(".highscore").textContent = highScore;
+        }
+
     } else if (guess > secretNumber) {
       
         if(score > 1) {
@@ -40,4 +50,17 @@ document.querySelector('.check').addEventListener
             document.querySelector(".score").textContent = 0;
         }
     }
+})
+
+document.querySelector('.again').addEventListener
+('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = "start guessing";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor ="#222";
+    document.querySelector('.number').style.width = '15rem'
 })
