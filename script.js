50daa19f8c1f32e9a08ab603ba8ac38d2859a70f
macbook air <macbookair@macbooks-MacBook-Air-2.local>
2024-01-17 04:02:01 +0400

    first commit

--- script.js
+++ script.js
@@ -20,10 +20,11 @@ document.querySelector('.check').addEventListener
     if(!guess){
         document.querySelector('.message').textContent = "no number selected"
     } else if(guess === secretNumber){
-        document.querySelector('.message').textContent = "Correct number";
+        document.querySelector('.message').textContent = "TELEPATIIIII <333";
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = "#60b347"
         document.querySelector('.number').style.width = "30rem"
+        document.querySelector(".h1").textContent = "me i guess i was a shoulder to cry onn";
 
         if(score > highScore){
             highScore = score;
